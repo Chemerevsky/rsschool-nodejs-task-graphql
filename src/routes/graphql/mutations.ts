8058305945d092d0ea5f1897ac79ef6009801c7d
Dima Chemerevsky <dchemerevsky@aras.com>
2024-03-04 17:40:33 +0100

    GraphQL. Delete post, user and profile

--- src/routes/graphql/mutations.ts
+++ src/routes/graphql/mutations.ts
@@ -7,6 +7,7 @@ import { PostType, CreatePostInputType } from './types/post.js';
 import { UserType, CreateUserInputType } from './types/user.js';
 import { ProfileType, CreateProfileInputType } from './types/profile.js'; 
 import { PrismaClient } from '@prisma/client';
+import { UUIDType } from './types/uuid.js';
 
 let prisma: PrismaClient;
 
@@ -24,6 +25,18 @@ const Mutation = new GraphQLObjectType({
                 return await prisma.post.create({ data: args.dto });
             }
         },
+        deletePost: {
+            type: GraphQLBoolean,
+            args: {
+                id: {
+                    type: UUIDType
+                },
+            },
+            resolve: async (_, { id }: { id: string }) => {
+                await prisma.post.delete({ where: { id }});
+                return true;
+            }
+        },
         createUser: {
             type: UserType,
             args: {
@@ -33,6 +46,18 @@ const Mutation = new GraphQLObjectType({
             },
             resolve: async (_, args: { dto }) => {
                 return await prisma.user.create({ data: args.dto });
+            }
+        },
+        deleteUser: {
+            type: GraphQLBoolean,
+            args: {
+                id: {
+                    type: UUIDType
+                },
+            },
+            resolve: async (_, { id }: { id: string }) => {
+                await prisma.post.delete({ where: { id }});
+                return true;
             }
         },
         createProfile: {
@@ -45,7 +70,19 @@ const Mutation = new GraphQLObjectType({
             resolve: async (_, args: { dto }) => {
                 return await prisma.profile.create({ data: args.dto });
             }
-        }
+        },
+        deleteProfile: {
+            type: GraphQLBoolean,
+            args: {
+                id: {
+                    type: UUIDType
+                },
+            },
+            resolve: async (_, { id }: { id: string }) => {
+                await prisma.post.delete({ where: { id }});
+                return true;
+            }
+        },
     }
 });
 
